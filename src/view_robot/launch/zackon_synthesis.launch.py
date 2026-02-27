82fcccf67a559e447189380dbd04b2ffd4045c35
buithanhthien <creeperthien@gmail.com>
2026-02-27 14:57:31 +0700

    updated new map

--- src/view_robot/launch/zackon_synthesis.launch.py
+++ src/view_robot/launch/zackon_synthesis.launch.py
@@ -20,9 +20,9 @@ def generate_launch_description():
     # ----------------------------------------------------
     
     # File Config & Map
-    nav2_params_file = PathJoinSubstitution([pkg_dir, 'config', 'nav2_params.yaml'])
+    nav2_params_file = PathJoinSubstitution([pkg_dir, 'config', 'nav2_params_update.yaml'])
     slam_params_file = PathJoinSubstitution([pkg_dir, 'config', 'mapper_params_online_async.yaml'])
-    map_file_path = PathJoinSubstitution([pkg_dir, 'maps', 'khoi_sofa_map3.yaml'])
+    map_file_path = PathJoinSubstitution([pkg_dir, 'maps', 'khoi_sofa_map4.yaml'])
 
     # Hardware Params
     lidar_frame_arg = DeclareLaunchArgument('lidar_frame', default_value='lidar_link')

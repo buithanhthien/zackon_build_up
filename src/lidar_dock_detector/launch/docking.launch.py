4a273abc76b4de4717b742cd3abd75f14b0405e0
buithanhthien <creeperthien@gmail.com>
2026-03-20 18:05:32 +0700

    update: remove commented unused docking node

--- src/lidar_dock_detector/launch/docking.launch.py
+++ src/lidar_dock_detector/launch/docking.launch.py
@@ -1,25 +1,3 @@
-# from launch import LaunchDescription
-# from launch_ros.actions import Node
-# from ament_index_python.packages import get_package_share_directory
-# import os
-
-
-# def generate_launch_description():
-#     params = os.path.join(
-#         get_package_share_directory('lidar_dock_detector'),
-#         'config', 'docking_params.yaml'
-#     )
-
-#     return LaunchDescription([
-#         Node(
-#             package='lidar_dock_detector',
-#             executable='docking_node',
-#             name='docking_node',
-#             output='screen',
-#             parameters=[params],
-#         ),
-#     ])
-
 import os
 from launch import LaunchDescription
 from launch_ros.actions import Node

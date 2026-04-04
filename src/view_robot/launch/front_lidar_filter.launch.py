4799a2d34626f61b6e9db2b4364a1a6fa4f68ac2
Khoa-AIot <hodangkhoa2710@gmail.com>
2026-04-04 14:23:17 +0700

    update: improve logic filter

--- src/view_robot/launch/front_lidar_filter.launch.py
+++ src/view_robot/launch/front_lidar_filter.launch.py
@@ -2,13 +2,16 @@ from launch import LaunchDescription
 from launch_ros.actions import Node
 
 def generate_launch_description():
+    pkg_dir = get_package_share_directory('view_robot_pkg')
+    config_file = os.path.join(pkg_dir, 'config', 'front_lidar_filter.yaml')
+
     return LaunchDescription([
         Node(
             package='laser_filters',
             executable='scan_to_scan_filter_chain',
             name='scan_front_filter',
             output='screen',
-            parameters=['/home/khoaiuh/zackon_ws/src/MangoMobileRobot/src/view_robot_pkg/config/front_lidar_filter.yaml'],
+            parameters=[config_file],
             remappings=[
                 ('scan', '/front_lidar/scan'),
                 ('scan_filtered', '/scan_front_lidar_filter'),
